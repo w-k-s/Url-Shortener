6f508e84570615b0fb2d38fa40a898758a25aa91
Waqqas Sheikh <wsheikh@tribalscale.com>
2019-07-30 17:20:09 +0400

    WIP: Move all configuration to app

--- app/app.go
+++ app/app.go
@@ -3,24 +3,69 @@ package app
 import (
 	"github.com/w-k-s/short-url/db"
 	"log"
+	"net/http"
+	"github.com/gorilla/mux"
 )
 
 type App struct {
 	logger     *log.Logger
 	db         *db.Db
+	server 	   *http.Server
+	router     *mux.Router
 	production bool
+	listeningAndServing bool
 }
 
-func Init(logger *log.Logger, connString string, production bool) *App {
+func Init() *App {
+	production := os.Getenv("PROD") == "1"
+
+	dbConnString := os.Getenv("MONGO_ADDRESS")
+	if len(dbConnString) == 0 {
+		dbConnString = "mongodb://localhost:27017/shorturl"
+	}
+
 	db := db.New(connString)
+
+	address := os.Getenv("ADDRESS")
+	if len(address) == 0 {
+		address = ":8080"
+	}
+	
+	router := mux.NewRouter()
+
+	server := createServer(, address)
+
+	logger := log.New(os.Stdout, "short-url: ", log.Ldate|log.Ltime)
+
 
 	app := &App{
 		logger,
 		db,
+		server,
+		router,
 		production,
+		false,
 	}
 
+	log.Printf("Address: '%s'", address)
+	log.Printf("Connection String: %s", dbConnString)
+	log.Printf("Production: %v", production)
+	log.Printf("Init Complete. Running on %s", address)
+
 	return app
+}
+
+func (a *App) ListenAndServe(errchan chan error){
+	go func(c chan error) {
+		
+		a.listeningAndServing = true
+		err := a.server.ListenAndServe()
+		a.listeningAndServing = false
+		
+		if err != nil {
+			errchan <- err
+		}
+	}(errchan)
 }
 
 func (a *App) Db() *db.Db {
@@ -37,4 +82,14 @@ func (a *App) IsProd() bool {
 
 func (a *App) Close() {
 	a.db.Close()
+}
+
+func createServer(h http.Handler, address string) *http.Server {
+	return &http.Server{
+		Handler: h,
+		Addr:    address,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+		IdleTimeout:  120 * time.Second,
+	}
 }
--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/gorilla/mux"
+	
 	"github.com/w-k-s/short-url/app"
 	"github.com/w-k-s/short-url/logging"
 	"github.com/w-k-s/short-url/urlshortener"
@@ -11,45 +11,23 @@ import (
 	"time"
 )
 
-var dbConnString string
-var address string
-var production bool
-var certDir string
-var domains map[string]bool
+var app app.App
 
 func init() {
-	production = os.Getenv("PROD") == "1"
-
-	address = os.Getenv("ADDRESS")
-	if len(address) == 0 {
-		address = ":8080"
-	}
-
-	dbConnString = os.Getenv("MONGO_ADDRESS")
-	if len(dbConnString) == 0 {
-		dbConnString = "mongodb://localhost:27017/shorturl"
-	}
-
-	log.Printf("Address: '%s'", address)
-	log.Printf("Connection String: %s", dbConnString)
-	log.Printf("Production: %v", production)
-	log.Printf("Init Complete. Running on %s", address)
+	app = app.Init()
 }
 
 func main() {
-	logger := log.New(os.Stdout, "short-url: ", log.Ldate|log.Ltime)
-	app := app.Init(logger, dbConnString, production)
 	defer app.Close()
 
 	logRepository := logging.NewLogRepository(app.Logger(), app.Db())
 
-	httpRouter := mux.NewRouter()
-	httpRouter.Use(loggingMiddleware(logRepository))
+	app.Middleware(loggingMiddleware(logRepository))
 
 	urlshortener.Configure(app, httpRouter)
 
 	errchan := make(chan error, 1)
-	listenAndServeHTTPServer(httpRouter, errchan)
+	app.ListenAndServe(errchan)
 	log.Fatalf("Error while configuring HTTP Server: %v", <-errchan)
 }
 
@@ -65,32 +43,5 @@ func loggingMiddleware(logRepository *logging.LogRepository) mux.MiddlewareFunc 
 
 			logRepository.LogResponse(sw, record)
 		})
-	}
-}
-
-//HTTP Server
-
-func listenAndServeHTTPServer(h http.Handler, errchan chan error) {
-
-	httpServer := createServer(h, address)
-	go func(c chan error) {
-		err := httpServer.ListenAndServe()
-		if err != nil {
-			errchan <- err
-		}
-	}(errchan)
-}
-
-//Utils
-
-func createServer(h http.Handler, addr string) *http.Server {
-	return &http.Server{
-		Handler: h,
-		Addr:    addr,
-		// set timeouts so that a slow or malicious client doesn't
-		// hold resources forever
-		ReadTimeout:  5 * time.Second,
-		WriteTimeout: 5 * time.Second,
-		IdleTimeout:  120 * time.Second,
 	}
 }
